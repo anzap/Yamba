37ef579cd97079f494a8b9785d05bb36cf3aa814
Andreas Zapantis <antreaszapantis@gmail.com>
2012-06-01 16:26:57 +0300

    status activity built

--- src/com/marakane/yamba/StatusActivity.java
+++ src/com/marakane/yamba/StatusActivity.java
@@ -8,6 +8,6 @@ public class StatusActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.status);
     }
 }
