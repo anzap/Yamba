dfd7b04d1148f6dd947b0ed759bed76abe7cba91
Andreas Zapantis <antreaszapantis@gmail.com>
2012-06-18 19:11:14 +0300

    Adding NetworkReceiver to start or stop the UpdaterService according to the network link status.

---
+++ src/com/marakane/yamba/NetworkReceiver.java
@@ -0,0 +1,26 @@
+package com.marakane.yamba;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+public class NetworkReceiver extends BroadcastReceiver {
+	private static final String TAG = NetworkReceiver.class.getSimpleName();
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		boolean isNetworkDown = intent.getBooleanExtra(
+				ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+
+		if (isNetworkDown) {
+			Log.d(TAG, "onReceive: Not connected, stopping UpdaterService");
+			context.stopService(new Intent(context, UpdaterService.class));
+		} else {
+			Log.d(TAG, "onReceive: connected, starting UpdaterService");
+			context.startService(new Intent(context, UpdaterService.class));
+		}
+	}
+
+}
